71ad065762ddc09c53438162086328ffc56e3620
Ivo Cenov <ivo.cenov1@gmail.com>
2019-11-26 22:20:24 +0200

    Init Commit

---
+++ Controllers/PinFormController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PINValidation.Models;
+
+namespace PINValidation.Controllers
+{
+    public class PinFormController : Controller
+    {
+
+        public IActionResult PinForm()
+        {
+            return View();
+        }
+    }
+}
