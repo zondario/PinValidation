f10d56c8ce8e218309c924d96bc16e07b40d2ecc
Ivo Cenov <ivo.cenov1@gmail.com>
2019-12-02 21:02:06 +0200

    remove unnecessary folders

---
+++ PINValidation.Tests/Validation/PinValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using PINValidation.Contracts.Validation;
+using PINValidation.Validation;
+using PINValidation.Validation.Sections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PINValidation.Tests.Validation
+{
+    public class PinValidatorTests
+    {
+        private PinValidator CreatePinValidator()
+        {
+            IList<IPinSectionValidator> validators = new List<IPinSectionValidator>();
+            validators.Add(new Length(0));
+            validators.Add(new ContentIsDigits(1));
+            validators.Add(new Dates(2));
+            validators.Add(new Checksum(3));
+            return new PinValidator(validators);
+        }
+        [Fact]
+        public void TestValidPin()
+        {
+            Assert.True(this.CreatePinValidator().IsValid("6101047500"));
+        }
+        [Fact]
+        public void TestInvalidPin()
+        {
+            Assert.False(this.CreatePinValidator().IsValid("6201047500"));
+        }
+    }
+}
---
+++ PINValidation.Tests/Validation/Sections/LengthTests.cs
@@ -0,0 +1,41 @@
+﻿using PINValidation.Contracts.Validation;
+using PINValidation.Validation;
+using PINValidation.Validation.Sections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PINValidation.Tests.Validation.Sections
+{
+    public class LengthTests
+    {
+        [Fact]
+        public void TestValidLength()
+        {
+            Length lengthValidator = new Length(0);
+            Assert.True(lengthValidator.IsValid("1234567890"));
+        }
+
+        [Fact]
+        public void TestGreaterLength()
+        {
+            Length lengthValidator = new Length(0);
+            Assert.False(lengthValidator.IsValid("12345678901"));
+        }
+
+        [Fact]
+        public void TestSmallerLength()
+        {
+            Length lengthValidator = new Length(0);
+            Assert.False(lengthValidator.IsValid("123456789"));
+        }
+
+        [Fact]
+        public void TestNull()
+        {
+            Length lengthValidator = new Length(0);
+            Assert.False(lengthValidator.IsValid(null));
+        }
+    }
+}
